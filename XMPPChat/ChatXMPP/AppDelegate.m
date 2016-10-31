4a529feda929c2281cc98adcb805b348bf32e0e5
kthree0830 <yvkqw@qq.com>
2016-10-31 11:18:15 +0800

    好久没用GitHub了

--- XMPPChat/ChatXMPP/AppDelegate.m
+++ XMPPChat/ChatXMPP/AppDelegate.m
@@ -17,6 +17,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    UIWindow * window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController * vc = [[UIViewController alloc]init];
+    window.rootViewController = vc;
+    [window makeKeyAndVisible];
     return YES;
 }
 
